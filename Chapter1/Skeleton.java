0cf95fac48651dd8de852548f1a74c9f80245855
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-16 22:26:47 +0800

    Committed 2021/09/16

--- Chapter1/Skeleton.java
+++ Chapter1/Skeleton.java
@@ -1,9 +1,46 @@
+// A JavaFx application skeleton.
 
-public class Skeleton {
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+
+public class Skeleton extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Launching JavaFX application.");
+		
+		// Start the JavaFX application by calling launch().
+		launch(args);
 	}
+	
+	public void init() {
+		System.out.println("Inside the init() method.");
+	}
+	
+	public void start(Stage myStage) {
+		System.out.println("Inside the start() method.");
+		
+		// Give the stage a title. 
+		myStage.setTitle("JavaFX Skeleton");
+		
+		// Create a root node. In this case, a flow layout is used, but several alternatives exist.
+		FlowPane rootNode = new FlowPane();
+		
+		// Create a scene
+		Scene myScene = new Scene(rootNode, 300, 200);
+		
+		// Set the scene on the stage. 
+		myStage.setScene(myScene);
+		
+		// Show the stage and its scene. 
+		myStage.show();
+		
+	}
+	
+	// Override the stop() method.
+	public void stop() {
+		System.out.println("Inside the stop() method.");
+	}	
 
 }
