e630d1566ac121ff510bf513cf85f7e2d0bf0602
Ceiling_roof <ted10014@hotmail.com>
2021-09-18 04:29:27 +0800

    Committed 2021/09/18

--- JavaFx_for_dummies/Click_me.java
+++ JavaFx_for_dummies/Click_me.java
@@ -1,14 +1,37 @@
 import javafx.application.*;
+import javafx.event.EventHandler;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 
-public class Click_me {
+public class Click_me extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		launch(args);
+	}
+	
+	public void init() {
+		
+	}
+	
+	Button button;
+	
+	public void start(Stage primaryStage) {
+		
+		button = new Button();
+		button.setText("Click me please!");
+		button.setOnAction(e -> buttonClick());
+		
+	}
+	
+	private Object buttonClick() {
+		if(button.getText() == "Click me please!");
+	}
 
+	public void stop() {
+		
 	}
 
 }
