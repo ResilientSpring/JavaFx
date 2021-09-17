2c003798c2f7e2dc23904a0c0bceb973bc5c86f5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-17 12:45:29 +0800

    Committed 2021/09/17

--- Chapter1/JavaFXSkeleton.java
+++ Chapter1/JavaFXSkeleton.java
@@ -4,7 +4,7 @@ import javafx.scene.*;
 import javafx.stage.*;
 import javafx.scene.layout.*;
 
-public class JavaFXSkeleton extends Application {
+public class JavaFXSkeleton extends Application { // A JavaFX application must be a subclass of the Application class.
 
 	public static void main(String[] args) {
 		System.out.println("Launching JavaFX application.");
@@ -18,7 +18,14 @@ public class JavaFXSkeleton extends Application {
 	}
 	
 	// The primary stage is supplied by the run-time system when a JavaFX application is started.
-	public void start(Stage myStage) {
+	public void start(Stage myStage) { // In actual stage play, a stage contains a scene.
+		// A stage is a container for scenes; a scene is a container for items that comprise the scene.
+		
+		System.out.println("Inside the start() method.");
+		
+		// Give the stage a title.
+		myStage.setTitle("JavaFX Skeleton");
+		
 		
 	}
 	
