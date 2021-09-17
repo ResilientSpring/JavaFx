468d3904a5e7de93af7ec5143bc05f8187cd5c5b
Ceiling_roof <ted10014@hotmail.com>
2021-09-18 04:09:28 +0800

    Committed 2021/09/18

---
+++ JavaFx_for_dummies/Click_me.java
@@ -0,0 +1,14 @@
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+public class Click_me {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
