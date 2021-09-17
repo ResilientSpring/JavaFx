b81d6a1b8919d0494c9e860074634f4e007ed666
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-17 11:48:05 +0800

    Committed 2021/09/17

---
+++ Chapter1/JavaFXSkeleton.java
@@ -0,0 +1,28 @@
+
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.layout.*;
+
+public class JavaFXSkeleton extends Application {
+
+	public static void main(String[] args) {
+		System.out.println("Launching JavaFX application.");
+		
+		// Launch JavaFX application by calling launch().
+		launch(args);
+	}
+	
+	public void init() {
+		System.out.println("Inside the initialization method.");
+	}
+	
+	public void start(Stage myStage) {
+		
+	}
+	
+	public void stop() {
+		
+	}
+
+}
