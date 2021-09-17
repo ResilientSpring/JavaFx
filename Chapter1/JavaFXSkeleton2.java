8a52fa3b9947fbc9e427f2c922d50710ab0cab9e
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-18 00:04:59 +0800

    Committed 2021/09/18

---
+++ Chapter1/JavaFXSkeleton2.java
@@ -0,0 +1,27 @@
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+
+public class JavaFXSkeleton2 extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void init() {
+		
+	}
+	
+	// The primary stage is supplied by the run-time system when a JavaFX application is started.
+	public void start(Stage primaryStage) {
+		
+	}
+	
+	public void stop() {
+		
+	}
+
+}
