fc56b71907d3920e5b0e24a601a59846256aabdf
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-19 01:07:19 +0800

    Committed 2021/09/18

--- Chapter1/SimpleSceneGraphDemo7.java
+++ Chapter1/SimpleSceneGraphDemo7.java
@@ -1,6 +1,8 @@
 import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.*;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 
 public class SimpleSceneGraphDemo7 extends Application {
 
@@ -13,7 +15,20 @@ public class SimpleSceneGraphDemo7 extends Application {
 	}
 	
 	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Simple Scene Graph Demo");
 		
+		FlowPane pane = new FlowPane();
+		
+		Label label = new Label("I am a label.");
+		Label label2 = new Label("I am another label.");
+		
+		pane.getChildren().addAll(label, label2);
+		
+		Scene scene = new Scene(pane, 300, 200);
+		
+		primaryStage.setScene(scene);
+		
+		primaryStage.show();
 	}
 	
 	public void stop() {
