9629222c0152cb5f04a0b5ba03aa974da42a8755
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-19 00:06:56 +0800

    Committed 2021/09/18

---
+++ Chapter1/SimpleSceneGraphDemo7.java
@@ -0,0 +1,23 @@
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+
+public class SimpleSceneGraphDemo7 extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void init() {
+		
+	}
+	
+	public void start(Stage primaryStage) {
+		
+	}
+	
+	public void stop() {
+		
+	}
+
+}
