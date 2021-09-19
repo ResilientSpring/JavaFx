f236a67f593384d992f67a987f3a69858f10ab03
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-19 19:49:18 +0800

    Committed 2021/09/19

--- JavaFx_for_dummies/ClickCounter.java
+++ JavaFx_for_dummies/ClickCounter.java
@@ -1,8 +1,15 @@
 import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 
 public class ClickCounter extends Application {
+	
+	Button button;
+	Label label;
+	int iClickCount = 0;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,8 +22,34 @@ public class ClickCounter extends Application {
 	
 	public void start(Stage primaryStage) {
 		
+		button = new Button("Click me, please!");
+		button.setOnAction(event -> buttonClick());
+		
+		label = new Label("You have not clicked the button.");
+		
+		BorderPane pane = new BorderPane();
+		pane.setTop(label);
+		pane.setCenter(button);
+		
+		Scene scene = new Scene(pane, 250, 150);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Click Counter");
+		
+		primaryStage.show();
+		
 	}
 	
+	public void buttonClick() {
+		iClickCount++;
+		if (iClickCount == 1) {
+			label.setText("You have clicked once.");
+		}
+		else {
+			label.setText("You have clicked " + iClickCount + " times.");
+		}
+	}
+
 	public void stop() {
 		
 	}
