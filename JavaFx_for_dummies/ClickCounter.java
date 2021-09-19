f3a8e550fa52d5f20df2f4a37c1b4cfcede6734b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-09-19 19:40:54 +0800

    Committed 2021/09/19

--- JavaFx_for_dummies/ClickCounter.java
+++ JavaFx_for_dummies/ClickCounter.java
@@ -1,9 +1,24 @@
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
 
-public class ClickCounter {
+public class ClickCounter extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		launch(args);
+	}
+	
+	public void init() {
+		
+	}
+	
+	public void start(Stage primaryStage) {
+		
+	}
+	
+	public void stop() {
+		
 	}
 
 }
