248ced0c607611af93b69e8330b49f2cc9931101
Ceiling_roof <ted10014@hotmail.com>
2021-10-13 01:59:51 +0800

    Committed 2021/10/12

--- Chapter2/JavaFxEventDemo2.java
+++ Chapter2/JavaFxEventDemo2.java
@@ -1,9 +1,16 @@
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
 
-public class JavaFxEventDemo2 {
+public class JavaFxEventDemo2 extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		launch(args);
+	}
+	
+	public void start(Stage primaryStage) {
+		
 	}
 
 }
