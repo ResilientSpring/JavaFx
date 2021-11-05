eb60d84002055111e4137dcbaa417041620ad729
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-06 03:19:29 +0800

    Committed 2021/11/6

---
+++ Chapter2/CheckBoxDemo.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class CheckBoxDemo extends Application {
+	
+	CheckBox keyboardCheckBox;
+	CheckBox mouseCheckBox;
+	CheckBox touchScreenCheckBox;
+	
+	Label responseLabel;
+	Label selectedLabel;
+	
+	String inputDeviceString = "";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
