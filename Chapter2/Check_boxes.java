2c4780d525abe7fe78e6de37892755c962adeb85
Ceiling_roof <ted10014@hotmail.com>
2021-11-06 04:06:32 +0800

    Committed 2021/11/5

--- Chapter2/Check_boxes.java
+++ Chapter2/Check_boxes.java
@@ -18,11 +18,15 @@ public class Check_boxes extends Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// Start the JavaFX application by calling launch().
+		launch(args);
 	}
 
 	@Override
 	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
+		
+		arg0.setTitle("Demonstrating Check Boxes");
 		
 	}
 
