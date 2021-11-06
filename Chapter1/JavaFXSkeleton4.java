11c5f4bf024b096c02cb39f78108ca605b7441c1
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-06 16:29:19 +0800

    Committed 2021/11/06

---
+++ Chapter1/JavaFXSkeleton4.java
@@ -0,0 +1,42 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class JavaFXSkeleton4 extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		launch(args);
+
+	}
+	
+	public void init() {
+		System.out.println("Inside the initialization method.");
+	}
+	
+	// The primary stage is supplied by the run-time system when a JavaFX application is started.
+	public void start(Stage primaryStage) { // In actual stage play, a stage contains a scene.
+		// A stage is a container for scenes; a scene is a container for items(elements/nodes) that comprise the scene.
+		
+		System.out.println("Inside the start() method.");
+		
+		primaryStage.setTitle("Give The Stage a Title");
+		
+		// To create a scene, you will first prepare elements, i.e nodes/items, to add to an instance of Scene.
+		// JavaFX offers several layout panes that manage how elements/nodes/items are placed in a scene.
+		FlowPane pane = new FlowPane();
+		
+		// A stage defines a space and a scene defines what goes in that space. 
+		Scene scene = new Scene(pane, 300, 200);
+		
+		// Set the scene on the stage.
+		primaryStage.setScene(scene);
+		
+		// show() shows the window that was created by the stage and scene. 
+		primaryStage.show();
+		
+	}
+
+}
