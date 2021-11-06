1b307a4a484ad1fa8e3500b4e37c9d99cbfd8eec
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-06 19:34:37 +0800

    Committed 2021/11/06

--- Chapter2/CheckBoxDemo.java
+++ Chapter2/CheckBoxDemo.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
@@ -30,6 +35,41 @@ public class CheckBoxDemo extends Application {
 		
 		// Vertical gap of 10.
 		FlowPane pane = new FlowPane(Orientation.VERTICAL, 0, 10);
+		
+		// Center the controls vertically; left-align them horizontally.
+		pane.setAlignment(Pos.CENTER_LEFT);
+		
+		// Set a padding value of 10 on the left for the flow pane.
+		pane.setPadding(new Insets(0, 0, 0, 10));
+		
+		// Create a scene. 
+		Scene scene = new Scene(pane, 300, 180);
+		
+		// Set the scene on the stage. 
+		primaryStage.setScene(scene);
+		
+		Label headingLabel = new Label("Select Input Devices.");
+		
+		// Create a label that will report the state of the selected check box.
+		responseLabel = new Label("Supported devices: <none>");
+		
+		// Create the check boxes. 
+		keyboardCheckBox = new CheckBox("Keyboard");
+		mouseCheckBox = new CheckBox("Mouse");
+		touchScreenCheckBox = new CheckBox("Touch Screen");
+		
+		// A source, such as check box, generates an event and sends it to one or more listeners, which handle the event.
+		// To receive an event, the handler for the event must first be registered with the event source.
+		keyboardCheckBox.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				// When the event occurs, the handler is called. It must then respond to the event and return.
+				
+				
+				
+			}
+		});
 		
 	}
 
