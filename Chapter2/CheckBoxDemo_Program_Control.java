2ff6b7785ec3c66d5e2e75487954ceadc24d161b
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-07 13:20:09 +0800

    Committed 2021/11/07

--- Chapter2/CheckBoxDemo_Program_Control.java
+++ Chapter2/CheckBoxDemo_Program_Control.java
@@ -1,9 +1,13 @@
 import javafx.application.Application;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class CheckBoxDemo_Program_Control extends Application {
+	
+	Button selectAllButton = new Button("Select All");
+	Button resetButton = new Button("Reset");
 	
 	CheckBox keyboardCheckBox = new CheckBox("Keyboard");
 	CheckBox mouseCheckBox = new CheckBox("Mouse");
