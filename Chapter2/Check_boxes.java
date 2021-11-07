d8e732bdd7205517921b13ceb0f2de5deed0c263
Ceiling_roof <ted10014@hotmail.com>
2021-11-07 12:14:09 +0800

    Committed 2021/11/07

--- Chapter2/Check_boxes.java
+++ Chapter2/Check_boxes.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
@@ -29,6 +31,48 @@ public class Check_boxes extends Application {
 		// TODO Auto-generated method stub
 		
 		arg0.setTitle("Demonstrating Check Boxes");
+		
+		checkBox_keyboard.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				if (checkBox_keyboard.isSelected()) {
+					responseLabel.setText("keyboard is selected.");
+				}
+				
+				if (checkBox_keyboard.isSelected() == false) {
+					responseLabel.setText("Keyboard is cleared.");
+				}
+			}
+		});
+		
+		checkBox_Mouse.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				if (checkBox_Mouse.isSelected()) {
+					responseLabel.setText("Mouse is selected.");
+				} else {
+					responseLabel.setText("Mouse is cleared.");
+				}
+			}
+		});
+		
+		checkBox_Touch.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				
+				if (checkBox_Touch.isSelected()) {
+					responseLabel.setText("Touch Screen is selected.");
+				} else {
+					responseLabel.setText("Touch Screen is cleared.");
+				}
+				
+			}
+		});
 		
 		FlowPane rootNodeFlowPane = new FlowPane(Orientation.VERTICAL, 0, 10);
 		
