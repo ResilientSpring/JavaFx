93b1ef9e34e2567707ba38cf1201ec26f37d7c74
Ceiling_roof <ted10014@hotmail.com>
2021-11-07 12:26:53 +0800

    Committed 2021/11/07

---
+++ Chapter2/Check_boxes2.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Check_boxes2 extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
