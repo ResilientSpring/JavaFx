9a969c36c1e5181f9ee5779c860cb78bbaba0cbb
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-01-17 15:09:09 +0800

    Committed 2021/01/17

--- Chapter2/Create_a_three_state_check_box.java
+++ Chapter2/Create_a_three_state_check_box.java
@@ -1,9 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Create_a_three_state_check_box {
+public class Create_a_three_state_check_box extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
